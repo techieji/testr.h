22ef5745329485e561c7fadd1164f1b8acdf31e0
Pradhyum R <drpradhyum2016@outlook.com>
2022-07-09 22:28:06 -0400

    Fixed a rookie mistake in the example file

--- testr_example.c
+++ testr_example.c
@@ -5,16 +5,12 @@ int arithmetic(char op, int n1, int n2) {
 	switch (op) {
 		case '+':
 			return n1 + n2;
-			break;
 		case '-':
 			return n1 - n2;
-			break;
 		case '*':
 			return n1 / n2;   // Purposeful mistake, can be fixed to show the contrast
-			break;
 		case '/':
 			return n1 / n2;
-			break;
 		default:
 			return -1;
 	}
